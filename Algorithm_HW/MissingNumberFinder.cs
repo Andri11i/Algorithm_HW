689a699c69d3c7e5b5cec668d7560eb02e7dbbf3
Andri11i <zotov.andrey.2008@gmail.com>
2024-10-31 10:12:22 +0200

    Add project files.

---
+++ Algorithm_HW/MissingNumberFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_HW
+{
+    using System;
+
+    static public class MissingNumberFinder
+    {
+        public static int FindMissingNumber(int[] nums, int n)
+        {
+      
+            int expectedSum = n * (n + 1) / 2;
+
+ 
+            int actualSum = 0;
+            foreach (int num in nums)
+            {
+                actualSum += num;
+            }
+
+            return expectedSum - actualSum;
+        }
+    }
+
+}
---
+++ Algorithm_HW/Program.cs
@@ -0,0 +1,7 @@
+﻿using Algorithm_HW;
+
+int[] nums = { 3, 7, 1, 2, 8, 4, 5 };
+int n = 8;
+
+int missingNumber = MissingNumberFinder.FindMissingNumber(nums, n);
+Console.WriteLine("Відсутнє число: " + missingNumber);
